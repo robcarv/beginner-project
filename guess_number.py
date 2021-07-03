46789f417cc5abbe63525c0ac5472798f59253c9
robcarv <78434639+robcarv@users.noreply.github.com>
2021-07-03 16:26:49 +0100

    adjust guess number  and  add rock  paper scissor

--- guess_number.py
+++ guess_number.py
@@ -4,7 +4,7 @@ def guess(x):
     random_number  = random.randint(1,x)
     guess = 0
     while guess != random_number:
-        guess = int(input(f"guess a number between 1 and {x}:"))
+        guess = int(input(f"guess a number between 1 and {x}: "))
         if guess < random_number :
             print ("Sorry, guess again ,too low")
         elif guess >random_number:
@@ -24,7 +24,7 @@ def computer_guess(x):
              guess =random.randint(low,high)
         else :
             guess = low        
-        feedback =input(f'is {guess}too hgh (H), too low (L), or correct (C) ???' ).lower()
+        feedback =input(f'is {guess} too hgh (H), too low (L), or correct (C) ' ).lower()
         if feedback  == 'h':
             high = guess -1
         elif feedback =='l':
---
+++ rock_paper_scissor.py
@@ -0,0 +1,30 @@
+import random
+
+def play ():
+
+    print ('-------------rules------------------\n')
+    print('     rock beat scissor\n     scissor beat paper \n     paper beat rock\n')
+    print ('------------------------------------')
+    print(' Choose one option ')
+    user  =  input("  'r' For rock\n  'p' for paper\n  's' for scissors\n").lower()
+    computer  = random.choice(['r','p','s'])
+
+    # def is_win(user,computer):
+    if (user == 'r' and computer =='s'):
+        print(f"you won {user} beat {computer}")
+    elif (user =='s' and computer =='p'):
+        print(f"you won {user} beat {computer}")
+    elif (user =='p' and computer =='r'):
+        print(f"you won {user} beat {computer}")  
+    elif user == computer:
+        print (f"Tie {user} = {computer}")
+    else:
+        print(f"computer winnn \n {computer} beat {user} ")
+    
+print(play())
+
+        
+        
+
+
+
